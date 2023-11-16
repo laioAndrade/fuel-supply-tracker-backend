cfddbb243fbc2e3df0f93cb2d7937531a1d7a212
laioAndrade <laioand98@gmail.com>
2023-11-15 21:39:34 -0300

    feat: add new validations to create

--- src/fuel-supply/fuel-supply.service.ts
+++ src/fuel-supply/fuel-supply.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { DriversService } from 'src/drivers/drivers.service';
+import { FuelTypesService } from 'src/fuel-types/fuel-types.service';
 import { Repository } from 'typeorm';
 import { CreateFuelSupplyDto } from './dto/createFuelSupply.dto';
 import { FuelSupply } from './fuel-supply.entity';
@@ -9,13 +11,29 @@ export class FuelSupplyService {
   constructor(
     @InjectRepository(FuelSupply)
     private fuelSupplys: Repository<FuelSupply>,
+    private driverService: DriversService,
+    private fuelTypeService: FuelTypesService,
   ) {}
 
-  createFuelSupply(fuelSupply: CreateFuelSupplyDto) {
-    this.fuelSupplys.save(fuelSupply);
+  async createFuelSupply(fuelSupply: CreateFuelSupplyDto) {
+    const { driver, fuelTypeId, fuelLevel } = fuelSupply;
+    const fuelType = await this.fuelTypeService.findOne(fuelTypeId);
+    const driverExist = await this.driverService.findOne(driver);
+    if (!driverExist) {
+      this.driverService.create({ cpf: driver });
+    }
+    const fuelSupplyPrice = parseFloat((fuelType.price * fuelLevel).toFixed(2));
+    const data = {
+      fuelSupplyPrice,
+      fuelType,
+      ...fuelSupply,
+    };
+    return this.fuelSupplys.save(data);
   }
 
-  findByDriver(driverId: number): string {
-    return 'Olá';
+  async findByDriver(driverCpf: string) {
+    const fuelSupplys = await this.fuelSupplys.findBy({ driver: driverCpf });
+
+    return fuelSupplys;
   }
 }
